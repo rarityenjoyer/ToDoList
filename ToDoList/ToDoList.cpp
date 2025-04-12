4a6d1baa730267b2cc76e47ff0c75cddc9e49bb3
m1zenn <rarityenjoyer@gmail.com>
2025-04-12 19:42:08 +0300

    Enhance task management usability and functionality
    - Updated `Task` constructor to include default values for `name` and `description`.
    - Rewrote `addTask` to use `std::getline` for multi-word input and initialized `completed` to false.
    - Improved `deleteTask` to allow task selection by number.
    - Modified `showTasks` to display task numbers starting from 1.
    - Corrected `statusUpdate` to properly access tasks based on user input.
    - Enhanced `interfaceFunction` for better user navigation and improved greeting message.

--- ToDoList/ToDoList.cpp
+++ ToDoList/ToDoList.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 struct Task
 {
@@ -8,7 +9,7 @@ struct Task
 	std::string description;
 	bool completed;
 
-	Task(std::string n, std::string d, bool c = 0) : name(n), description(d), completed(c) {}
+	Task(std::string n = "Task", std::string d = "Default Task", bool c = 0) : name(n), description(d), completed(c) {}
 };
 
 bool operator==(const Task& lhs, const Task& rhs)
@@ -16,72 +17,74 @@ bool operator==(const Task& lhs, const Task& rhs)
 	return lhs.name == rhs.name && lhs.description == rhs.description && lhs.completed == rhs.completed;
 }
 
-void addTask(std::vector<Task>& tasks)
-{
-	std::string name;
-	std::string description;
-	bool completed;
-	std::cout << "Give a short name for a task: \n";
-	std::cout << "Example: 'wash dishes', 'clean up my room', 'go to the gym'\n";
-	std::cin >> name;
-	std::cout << "Good! Now give it a more detailed description if it's needed.\n";
-	std::cout << "(If you want to skip this part - just press <ENTER> button.)\n";
-	std::cin >> description;
-	std::cout << "Done! Your task is created.\n";
-	Task task(name, description, completed);
-	tasks.push_back(task);
-}
-
-void deleteTask(std::vector<Task>& tasks, Task task)						// doesn't works properly yet.
-{
-	auto it = std::find(tasks.begin(), tasks.end(), task);
-	if (it != tasks.end()) 
-	{
-		tasks.erase(it);
-	}
-	else 
-	{
-		std::cout << "Task not found" << std::endl;
-	}
-}
-
 const void showTasks(std::vector<Task>& tasks)
 {
 	for (int count = 0; count < tasks.size(); ++count)
 	{
-		std::cout << "Task #" << count << std::endl;
+		std::cout << "Task #" << count + 1 << std::endl;
 		std::cout << "Name: " << tasks[count].name << std::endl;
 		std::cout << "Description: " << tasks[count].description << std::endl;
 		std::cout << "Status: " << ((tasks[count].completed == true) ? "Completed\n" : "Uncompleted\n") << std::endl;
 	}
 }
 
-void statusUpdate(std::vector<Task>& tasks)						
+void deleteTask(std::vector<Task>& tasks)		// doesn't work properly yet.
+{
+	showTasks(tasks);
+	std::cout << "Choose the number of the task that you want to delete: ";
+	int taskNumber = 0;
+	std::cin >> taskNumber;
+	tasks.erase(tasks.begin() + taskNumber - 1);
+	std::cout << "You succesfully deleted task #" << taskNumber << ".\n";
+}
+
+void statusUpdate(std::vector<Task>& tasks)		// doesn't work properly yet.				
 {
 	showTasks(tasks);
 	std::cout << "Choose the number of the task that you want to change the status of: ";
 	int taskNumber = 0;
 	std::cin >> taskNumber;
-	tasks[taskNumber].completed = true;
+	tasks[taskNumber-1].completed = true;
 	std::cout << "Good work! Congratulations on completing the '" << tasks[taskNumber].name << "' task.\n";
 }
 
+void addTask(std::vector<Task>& tasks)
+{
+	std::string name;
+	std::string description;
+	bool completed = 0;
+	std::cout << "Give a short name for a task: \n";
+	std::cout << "Example: 'wash dishes', 'clean up my room', 'go to the gym'\n";
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	std::getline(std::cin, name);
+	std::cout << "Good! Now give it a more detailed description if it's needed.\n";
+	std::cout << "(If you want to skip this part - just press <ENTER> button.)\n";
+	std::getline(std::cin, description);
+	std::cout << "Done! Your task is created.\n";
+	Task task(name, description, completed);
+	tasks.push_back(task);
+}
+
+
 void interfaceFunction(std::vector<Task> tasks, std::string name)
 {
-	std::cout << "Hi there, " << name << "! " << ((tasks.size() == 0) ? "You haven't any tasks yet ;(\n" : "Hope you completed all your tasks for today!\n");
-	std::cout << "Use 1-5 for navigation through the interface <3\n";
-	std::cout << "1) View all of my tasks.\n";
-	std::cout << "2) Add a new task.\n";
-	std::cout << "3) Delete task.\n";
-	std::cout << "4) Update a status of your task.\n";
-	std::cout << "5) Exit.\n";
-	int usersChoice = 0;
-	while (usersChoice < 1 || usersChoice > 5)
+	std::cout << "Hey, " << name << "! " << ((tasks.size() == 0) ? "Looks like you don't have any tasks yet ;(\n" : "Hope you completed all your tasks for today!\n");
+	int workStatus = 1;
+	while(workStatus)
 	{ 
-		std::cin >> usersChoice;
-	}
-	switch (usersChoice)
-	{
+		std::cout << "Use 1-5 for navigation through the interface <3\n";
+		std::cout << "1) View all of my tasks.\n";
+		std::cout << "2) Add a new task.\n";
+		std::cout << "3) Delete task.\n";
+		std::cout << "4) Update a status of your task.\n";
+		std::cout << "5) Exit.\n";
+		int usersChoice = 0;
+		while (usersChoice < 1 || usersChoice > 5)
+		{ 
+			std::cin >> usersChoice;
+		}
+		switch (usersChoice)
+		{
 		case 1:
 			showTasks(tasks);
 			break;
@@ -95,7 +98,9 @@ void interfaceFunction(std::vector<Task> tasks, std::string name)
 			statusUpdate(tasks);
 			break;
 		case 5:
+			workStatus = 0;
 			break;
+		}
 	}
 }
 
